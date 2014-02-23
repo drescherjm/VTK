drescherjm/VTK

commit 7f08be80c60f3ab761155fd03a91077aa6b22ef0

BUG: InteractorStyleImage not getting image for Window/Level.
If an observer has not been set for the StartWindowLevelEvent, then
the vtkInteractorStyleImage is supposed to automatically locate the
last vtkImageActor or vtkImageSlice that was added to the renderer,
and apply the window/level to that actor.  However, there was a bug
where if a non-image actor was added to the renderer after the image
actor, the image actor would not be found.  This patch fixes that bug.

Backport of 022d847a from master to release.

Change-Id: I58baac006ffe66ef367e196c10f98e60ee6e270e

Rendering/vtkInteractorStyleImage.cxx
@@ -89,6 +89,10 @@ void vtkInteractorStyleImage::StartWindowLevel()
     return;
     }
   this->StartState(VTKIS_WINDOW_LEVEL);
+
+  // Get the last (the topmost) image
+  this->SetCurrentImageToNthImage(-1);
+
   if (this->HandleObservers &&
       this->HasObserver(vtkCommand::StartWindowLevelEvent))
     {
@@ -96,9 +100,6 @@ void vtkInteractorStyleImage::StartWindowLevel()
     }
   else
     {
-    // Get the last (the topmost) image
-    this->SetCurrentImageToNthImage(-1);
-
     if (this->CurrentImageProperty)
       {
       vtkImageProperty *property = this->CurrentImageProperty;
@@ -642,15 +643,24 @@ void vtkInteractorStyleImage::SetCurrentImageToNthImage(int i)
     int j = 0;
     for (props->InitTraversal(pit); (prop = props->GetNextProp(pit)); )
       {
+      bool foundImageProp = false;
       for (prop->InitPathTraversal(); (path = prop->GetNextPath()); )
         {
         vtkProp *tryProp = path->GetLastNode()->GetViewProp();
         if ( (imageProp = vtkImageSlice::SafeDownCast(tryProp)) != 0 )
           {
-          if (j == i) { break; }
+          if (j == i)
+            {
+            foundImageProp = true;
+            break;
+            }
           imageProp = 0;
           j++;
           }
+        }
+      if (foundImageProp)
+        {
+        break;
         }
       }
     if (i < 0)

Rendering/vtkInteractorStyleImage.h
@@ -156,6 +156,14 @@ public:
   void SetImageOrientation(const double leftToRight[3],
                            const double bottomToTop[3]);
 
+  // Description:
+  // Get the current image property, which is set when StartWindowLevel
+  // is called immediately before StartWindowLevelEvent is generated.
+  // This is the image property of the topmost vtkImageSlice in the
+  // renderer or NULL if no image actors are present.
+  vtkImageProperty *GetCurrentImageProperty() {
+    return this->CurrentImageProperty; }
+
 protected:
   vtkInteractorStyleImage();
   ~vtkInteractorStyleImage();

commit e6bc16a44d9df0a0ed409054a22dab4dd6702529

BUG_FIX: Revert part of WindowLevel fix.
Commit 7f08be80c60f3ab761155fd03a91077aa6b22ef0 caused Window and
Level to not work on several of my applications. This commit reverses the fix.

Rendering/vtkInteractorStyleImage.cxx
@@ -90,8 +90,7 @@ void vtkInteractorStyleImage::StartWindowLevel()
     }
   this->StartState(VTKIS_WINDOW_LEVEL);
 
-  // Get the last (the topmost) image
-  this->SetCurrentImageToNthImage(-1);
+
 
   if (this->HandleObservers &&
       this->HasObserver(vtkCommand::StartWindowLevelEvent))
@@ -100,6 +99,9 @@ void vtkInteractorStyleImage::StartWindowLevel()
     }
   else
     {
+    // Get the last (the topmost) image
+    this->SetCurrentImageToNthImage(-1);
+    
     if (this->CurrentImageProperty)
       {
       vtkImageProperty *property = this->CurrentImageProperty;